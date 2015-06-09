4c27edb7b333023d13edf5f01a069e3b16ac4de9
Son Truong <sont85@gmail.com>
2015-06-08 20:03:48 -0700

    coderbyteMedium Permutation

--- CodingHouse/test/coderbyteMedium.js
+++ CodingHouse/test/coderbyteMedium.js
@@ -1,15 +1,17 @@
 function assertEqual(result, answer) {
- 	 console.log(result === answer? true: false)
+   console.log(result === answer? true: false)
 }
 
 
 
 function PermutationStep(num) { 
-	num = num.toString().split("")
-	console.log(num)
+  num = num.toString().split("")
+  console.log(num)
+  var higherNumber = 0 
+  for (var i = num.length -1; 1 >= 0; i--) {
+    if (num[i] > higherNumber){
+      return true
+    }
+  }
 }
-   
-assertEqual(PermutationStep(11121), 11211)
-
-
-
+console.log(PermutationStep(11121))
