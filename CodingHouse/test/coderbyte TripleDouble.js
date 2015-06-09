082f9b568f95860115cc3c1fe15f70f2d588e364
Son Truong <sont85@gmail.com>
2015-06-08 20:23:42 -0700

    assert equal TripleDouble Coderbyte

---
+++ CodingHouse/test/coderbyte TripleDouble.js
@@ -0,0 +1,9 @@
+function assertEqual(result, answer) {
+   console.log(result === answer? true: false)
+}
+
+function TripleDouble(num1, num2) {
+	
+}
+
+assertEqual(TripleDouble(451999277, 41177722899), 1)
