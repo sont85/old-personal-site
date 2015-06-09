aaab029bad054989ba60a504572bce2d3dd01e58
Son Truong <sont85@gmail.com>
2015-06-08 19:41:47 -0700

    Coderbyte TDD, create assert

---
+++ CodingHouse/test/coderbyteMedium.js
@@ -0,0 +1,15 @@
+function assertEqual(result, answer) {
+ 	 console.log(result === answer? true: false)
+}
+
+
+
+function PermutationStep(num) { 
+	num = num.toString().split("")
+	console.log(num)
+}
+   
+assertEqual(PermutationStep(11121), 11211)
+
+
+
