eebf616dd1bdb40d283149a3e2954ab85a7057e9
Son Truong <sont85@gmail.com>
2015-06-08 20:34:30 -0700

    return true for triple digit

--- CodingHouse/test/coderbyteMedium.js
+++ CodingHouse/test/coderbyteMedium.js
@@ -10,6 +10,7 @@ function PermutationStep(num) {
   var higherNumber = 0 
   for (var i = num.length -1; 1 >= 0; i--) {
     if (num[i] > higherNumber){
+    	console.log(num[i])
       return true
     }
   }
