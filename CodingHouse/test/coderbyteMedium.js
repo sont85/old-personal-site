2cbcd77ed813d10ddfae2e26b24ed8bf3e15eea2
Son Truong <sont85@gmail.com>
2015-06-08 21:00:41 -0700

    Problem Solved Bracket Matcher

--- CodingHouse/test/coderbyteMedium.js
+++ CodingHouse/test/coderbyteMedium.js
@@ -2,12 +2,16 @@ function assertEqual(result, answer) {
    console.log(result === answer? true: false)
 }
 
-
-
 function BracketMatcher(str) {
   var leftBracket = str.match(/[(]/g)
   var rightBracket = str.match(/[)]/g)
-  console.log(leftBracket,rightBracket)
+  if (!leftBracket && !rightBracket) {
+    return 1
+  } else if (!leftBracket || !rightBracket) {
+    return 0
+  } else {
+	return leftBracket.length === rightBracket.length? 1: 0;
+  }
 }
 
 assertEqual(BracketMatcher("(coder)(byte))"))
