52fe422cfe38139f054d409e127483d20aeff694
Son Truong <sont85@gmail.com>
2015-06-08 21:02:27 -0700

    found triple digit

--- CodingHouse/test/coderbyte TripleDouble.js
+++ CodingHouse/test/coderbyte TripleDouble.js
@@ -1,9 +1,17 @@
 function assertEqual(result, answer) {
-   console.log(result === answer? true: false)
+  console.log(result === answer? true: false)
 }
 
 function TripleDouble(num1, num2) {
-	
+  var answer =  num1.toString().split("").some(function (item, index, array) {
+    var isTriple = item === array[index + 1] && item === array[index + 2]
+
+    console.log(isTriple)
+
+});
+  return answer? 1: 0;
+  
 }
+
 
 assertEqual(TripleDouble(451999277, 41177722899), 1)
