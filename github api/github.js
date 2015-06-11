d423915d6d85881e38dcad589fd415ce55efa62c
Son Truong <sont85@gmail.com>
2015-06-10 19:25:32 -0700

    small changes to github

--- github api/github.js
+++ github api/github.js
@@ -1,27 +1,20 @@
 $(function() {
     var userName = []
-    
+
     if (localStorage.names) {
         userName = JSON.parse(localStorage.names)
-        console.log(userName)
         userName.forEach(function (item, index, array) {
             ajax(item)
         })
-        
-        
     }
     
     function buildHtml(name, imageUrl) {
         var cloneUser = $(".user:first").clone(true).show();
-  
         cloneUser.find(".user-name").text(name)
         cloneUser.find(".user-image").prop("src", imageUrl )
         $("#github").append(cloneUser)
-        console.log(name)
-        console.log(imageUrl)
         
     }
-    
     
     function ajax(name) {
             $.ajax("https://api.github.com/users/" + name)
@@ -29,13 +22,13 @@ $(function() {
                 buildHtml(response.name, response.avatar_url)
                 userName.push(name)
                 localStorage.setItem("names", JSON.stringify(userName))
-            });
+            })
+            .error(function (response) {
+                console.log(response)
+            })
     }
-    
     $("#searchButton").click(function() {
         var name = $("#searchText").val()
-        var nameapi = "https://api.github.com/users/" + name
-        console.log("https://api.github.com/users/" + name)
         ajax(name)
     })
 });
