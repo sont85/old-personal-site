d1ed27c0dd0bcfa91a4f500662035ed1a77ea376
Son Truong <sont85@gmail.com>
2015-06-21 15:51:43 -0700

    fixed bugs on local Storage

--- Contact List/table.js
+++ Contact List/table.js
@@ -3,8 +3,11 @@ var app = angular.module("contact", ["angular-md5"]);
 
 app.controller("mainCtrl", function(md5) {
   cScope = this;
-  var localStorageContacts = localStorage.getItem("ContactList")
-  cScope.contactList = JSON.parse(localStorageContacts) || [{"firstName":"Son","lastName":"Truong","email":"sont85@gmail.com","phoneNumber":"707-246-1547","gravatar":"http://www.gravatar.com/avatar/96d6b5ec926ebc3ecdc731eb03bf7aa6"}]
+  var localStorageContacts = JSON.parse(localStorage.getItem("ContactList")) 
+  if (localStorageContacts.join() === "") {
+    localStorageContacts = [{firstName: "Son", lastName: "Truong", email: "sont85@gmail.com", phoneNumber: "707-246-1547", gravatar: "http://www.gravatar.com/avatar/96d6b5ec926ebc3ecdc731eb03bf7aa6"}]
+  }
+  cScope.contactList = localStorageContacts
 
   cScope.headerList = Object.keys(cScope.contactList[0])
   cScope.addContact = function(contact) {
