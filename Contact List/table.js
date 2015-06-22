aeb1bfdd58d51cb6c3ceddb27550e8750e8b068b
Son Truong <sont85@gmail.com>
2015-06-21 18:26:35 -0700

    added query selector and initial width

--- Contact List/table.js
+++ Contact List/table.js
@@ -2,8 +2,13 @@
 var app = angular.module("contact", ["angular-md5"]);
 
 app.controller("mainCtrl", function(md5) {
+
+   if (document.documentElement.clientWidth > 767) {
+      document.querySelector("meta[name=viewport]").setAttribute('content', 'initial-scale=1');
+    };
+
   cScope = this;
-  var defaultData = [{First_Name: "Son", Last_Name: "Truong", Email: "sont85@gmail.com", Phone: "707-246-1547", gravatar: "http://www.gravatar.com/avatar/96d6b5ec926ebc3ecdc731eb03bf7aa6"}]
+  var defaultData = [{First_Name: "Son", Last_Name: "Truong", Email: "sont85@gmail.com", Phone: "707-246-1547", Gravatar: "http://www.gravatar.com/avatar/96d6b5ec926ebc3ecdc731eb03bf7aa6"}]
   var localStorageContacts = JSON.parse(localStorage.getItem("ContactList")) 
   if (localStorageContacts === null ) {
     cScope.contactList = defaultData
@@ -16,14 +21,13 @@ app.controller("mainCtrl", function(md5) {
   cScope.headerList = Object.keys(defaultData[0])
   cScope.addContact = function(contact) {
     var copyContact = angular.copy(contact);
-    copyContact.gravatar = cScope.addGravatar(contact.email);
+    copyContact.Gravatar = cScope.addGravatar(contact.email);
     cScope.contactList.push(copyContact);
     localStorage.setItem("ContactList", JSON.stringify(cScope.contactList));
   }
 
   cScope.addGravatar = function(email) {
     cScope.hash = md5.createHash(email || "");
-    console.log(cScope.gravatar);
     return "http://www.gravatar.com/avatar/"+cScope.hash;
   }
   cScope.sort = function(header) {
